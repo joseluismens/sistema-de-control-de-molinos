6536eb623e4791497cca79419a7644df009aa38e
Manuel Ponce <manuelponce147@gmail.com>
2020-08-26 04:04:45 -0400

    add DB online

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,7 +1,11 @@
 import { connect } from 'mongoose'
 
 export async function startConnection() {
-    const db = await connect('mongodb://localhost/molinos', {
+
+    const uri = "mongodb://manuel:manuel@molinos-shard-00-00.93qfn.mongodb.net:27017,molinos-shard-00-01.93qfn.mongodb.net:27017,molinos-shard-00-02.93qfn.mongodb.net:27017/molinos?ssl=true&replicaSet=atlas-wxwpzt-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+
+    const db = await connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
